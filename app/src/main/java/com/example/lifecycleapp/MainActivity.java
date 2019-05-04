6586201e9742819be55729aac199fc784c788c2d
gmmha <projectvid2018@gmail.com>
2019-05-04 20:46:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lifecycleapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.lifecycleapp;
+
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String TAG =  "mainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"OnCreate Called",Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"OnCreate Called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this,"onStart Called",Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"onStart Called");
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Toast.makeText(MainActivity.this,"onPostResume",Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"onPostResume Called");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this,"onPause",Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"onPause Called");
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this,"onStop Called",Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"onStop Called");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this,"onDestroy Called",Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"onDestroy Called");
+
+    }
+
+}
